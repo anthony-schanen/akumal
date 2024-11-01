5213b4d98bd96b69ff42d366997e26bfcc6d90e1
anthony-schanen-BAB <schanenanthony@gmail.com>
2024-11-01 16:13:03 -0700

    removed analytics api

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
-"use client"
+"use client";
+
 import { useState } from 'react';
-import styles from '@/app/styles/RegexValidation.module.css';
+import styles from '@/app/styles/RegexValidation.module.css'
 
 export default function RegexValidation() {
   const [input, setInput] = useState('');
   const [isValid, setIsValid] = useState(false);
 
+  // Validate input using regex
   const validateInput = (value) => {
     const pattern = /^(?:\d \d{2} \d{4}\/\d{2}\/\d{2} \d{2}:\d{2}\n?)+$/;
     setIsValid(pattern.test(value));
@@ -17,38 +19,24 @@ export default function RegexValidation() {
     validateInput(value);
   };
 
-  const handleSubmit = async () => {
-    try {
-      const response = await fetch('/api/analytics', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ data: input }),
-      });
-      // Handle response
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
   return (
-    <div className={styles.container}>
-      <textarea
-        id="inputField"
-        placeholder="Enter data"
-        value={input}
-        onChange={handleInputChange}
-        className={styles.inputField}
-      ></textarea>
-      <button
-        id="submitButton"
-        disabled={!isValid}
-        onClick={handleSubmit}
-        className={`${styles.submitButton} ${isValid ? styles.enabled : ''}`}
-      >
-        Submit
-      </button>
+    <div className={styles.outerContainer}>
+      <div className={styles.container}>
+        <textarea
+          id="inputField"
+          placeholder="Enter data"
+          value={input}
+          onChange={handleInputChange}
+          className={styles.inputField}
+        ></textarea>
+        <button
+          id="submitButton"
+          disabled={!isValid}
+          className={`${styles.submitButton} ${isValid ? styles.enabled : ''}`}
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
